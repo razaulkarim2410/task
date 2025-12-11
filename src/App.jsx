5d65e658b1c1b1c6e31dc1305fb0b131ee669b92
Razaul Karim <razaulkarim2410@gmail.com>
2025-12-12 01:14:22 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Body from './components/Body/Body'
+import Navbar from './components/navbar/Navbar'
+import PaymentGatewayPage from './components/PaymentGatewayPage'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    
+      <PaymentGatewayPage></PaymentGatewayPage>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Body = () =>{
+    return (
+        <div >
+    <div></div>
+    <div>
+        <p>WHMCH/Extnsion Modules / Payments & Billing</p>
+        <h1>Payment Gateway Charges For WHMCS</h1>
+        <div>
+           <div></div> 
+           <div></div> 
+           <div></div> 
+           <div></div> 
+           <div></div> 
+           <div></div> 
+        </div>
+
+    </div>
+</div>
+    )
+}
+export default Body;
